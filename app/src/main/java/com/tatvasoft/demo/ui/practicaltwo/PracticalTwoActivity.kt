a526d85ea46042b6e633bdcb6449ee674ecf09e1
Chintan <chintan.prajapati04@gmail.com>
2021-12-29 19:00:25 +0530

    Practical Two * Add Glide * Pagination

--- app/src/main/java/com/tatvasoft/demo/ui/practicaltwo/PracticalTwoActivity.kt
+++ app/src/main/java/com/tatvasoft/demo/ui/practicaltwo/PracticalTwoActivity.kt
@@ -1,9 +1,10 @@
 package com.tatvasoft.demo.ui.practicaltwo
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tatvasoft.demo.R
 import com.tatvasoft.demo.base.BaseActivity
 import com.tatvasoft.demo.base.Result
@@ -14,6 +15,7 @@ import kotlinx.android.synthetic.main.activity_practical_two.*
 class PracticalTwoActivity : BaseActivity(R.layout.activity_practical_two) {
 
     private lateinit var dataViewModel: DataViewModel
+    var loading: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +23,44 @@ class PracticalTwoActivity : BaseActivity(R.layout.activity_practical_two) {
         rvData.adapter = adData
         rvData.layoutManager = LinearLayoutManager(this)
 
-        dataViewModel = ViewModelProvider(this).get(DataViewModel::class.java)
-        dataViewModel.getData()
+        dataViewModel = ViewModelProvider(this)[DataViewModel::class.java]
+
+        rvData.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0) {
+                    val visibleCount = (rvData.layoutManager as LinearLayoutManager).childCount
+                    val totalCount = (rvData.layoutManager as LinearLayoutManager).itemCount
+                    val pastVisibleItems =
+                        (rvData.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                    if (loading) {
+                        if ((visibleCount + pastVisibleItems) >= totalCount) {
+                            loading = false
+                            dataViewModel.getData().apply {
+                                pbLoading.visibility = View.VISIBLE
+                            }
+                        }
+                    }
+                }
+
+            }
+        })
+
+        dataViewModel.getData().apply {
+            pbLoading.visibility = View.VISIBLE
+        }
 
         dataViewModel.mData.observe(this, {
+            pbLoading.visibility = View.GONE
             when (it) {
                 is Result.Success -> {
-                    adData.addData(it.data)
+                    loading = true;
+                    adData.addData(it.data.data)
 
                 }
                 is Result.Error -> {
-
+                    //If you to set error depends on your requirement
+                    makeToast("Something went wrong.!!")
                 }
             }
         })
--- app/src/main/java/com/tatvasoft/demo/ui/practicaltwo/adapter/DataAdapter.kt
+++ app/src/main/java/com/tatvasoft/demo/ui/practicaltwo/adapter/DataAdapter.kt
@@ -4,31 +4,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.tatvasoft.demo.R
 import com.tatvasoft.demo.model.Data
+import kotlinx.android.synthetic.main.lay_data_item.view.*
 
 class DataAdapter : RecyclerView.Adapter<DataAdapter.DataHolder>() {
-    private var alData =  ArrayList<Data>()
+    private var alData = ArrayList<Data>()
 
-    class DataHolder(view : View) : RecyclerView.ViewHolder(view) {
+    class DataHolder(view: View) : RecyclerView.ViewHolder(view) {
 
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataHolder {
-        return DataHolder(LayoutInflater.from(parent.context).inflate(R.layout.lay_data_item, parent, false))
+        return DataHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.lay_data_item, parent, false)
+        )
 
     }
 
     override fun onBindViewHolder(holder: DataHolder, position: Int) {
-
+        val item = alData[position]
+        holder.itemView.tvName.text = item.firstName.plus(" ").plus(item.lastName)
+        holder.itemView.tvEmail.text = item.email
+        Glide.with(holder.itemView.context)
+            .load(item.avatar)
+            .circleCrop()
+            .into(holder.itemView.ivPic)
     }
 
     override fun getItemCount(): Int {
-        return  0
+        return alData.size
     }
 
-    fun addData(list : ArrayList<Data>){
-
+    fun addData(list: ArrayList<Data>) {
+        alData = list
+        notifyDataSetChanged()
     }
 }
